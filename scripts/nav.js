09a7176559879cf519478091bc793eaab7a6a6ea
chirani-kohomban <chirani.vihanxa@gmail.com>
2025-04-25 03:56:49 +0530

    Add hamburger menu JS and fixed mobile navbar

---
+++ scripts/nav.js
@@ -0,0 +1,10 @@
+console.log("JS is loaded");
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+  
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+  });
+  
